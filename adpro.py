ecfde72eafb34d6d14415e8b06993fd5616217e9
Marco Del Tutto <marco.deltutto@gmail.com>
2023-07-28 06:24:02 -0500

    first commit

---
+++ adpro.py
@@ -0,0 +1,82 @@
+import time
+
+from WF_SDK import device, scope, wavegen, tools, error
+
+class ADPro():
+
+    counter = 0
+
+    _device_data = None
+    _buffer = None
+    _lamp_frequency = 1 # Hz
+    _is_flashing = False
+
+    def __init__(self):
+        print('myclass __init__')
+        self._device_data = device.open()
+
+    def lamp_on(self):
+        print('Turning lamp on')
+        wavegen.generate(self._device_data,
+                         channel=1,
+                         function=wavegen.function.square,
+                         offset=1,
+                         frequency=self._lamp_frequency,
+                         amplitude=1)
+        self._is_flashing = True
+
+    def lamp_off(self):
+        print('Turning lamp off')
+        wavegen.close(self._device_data)
+
+    def set_lamp_frequency(self, frequency):
+        _lamp_frequency = frequency # Hz
+        if _is_flashing:
+            self.lamp_off()
+            self.lamp_on()
+
+    def start_capture(self):
+        # self._buffer = scope.record(device_data, channel=1)
+        return scope.start_capture(self._device_data)
+
+    def check_capture(self):
+        return scope.check_capture(self._device_data)
+
+    def get_data(self):
+        self._buffer = scope.get_data(self._device_data, [1, 2])
+        return self._buffer
+
+    def close(self):
+        device.close(self._device_data)
+
+    # def get_data(self):
+    #     return self._buffer
+
+    def print_f(self):
+        print('Hi! print_f')
+
+    def count(self):
+        self.counter += 1
+        print('counter', self.counter)
+
+
+if __name__ == "__main__":
+    digitizer = ADPro()
+
+    print('About to call lamp_on')
+    digitizer.lamp_on()
+    print('Called lamp_on')
+    time.sleep(10)
+    print('About to call lamp_off')
+    digitizer.lamp_off()
+    print('Called lamp_off')
+
+    digitizer.close()
+
+    # digitizer.start_capture()
+    # while not digitizer.check_capture():
+    #     time.sleep(1)
+    # print(digitizer.get_data())
+
+
+
---
+++ main.py
@@ -0,0 +1,66 @@
+# main.py
+
+import time
+
+from fastapi import FastAPI
+from adpro import ADPro
+
+app = FastAPI()
+
+_digitizer = ADPro()
+
+@app.get("/")
+async def root():
+    # mc.count()
+    return {"message": "Hello World"}
+
+
+@app.get("/lamp_control/{status}")
+async def lamp_control(status):
+    s = status.lower() in ['on', 'true', '1', 't', 'y', 'yes']
+    if s:
+        print('Turn lamp on')
+        _digitizer.lamp_on()
+    else:
+        print('Turn lamp off')
+        _digitizer.lamp_off()
+    return {"status": status}
+
+@app.get("/lamp_control/frequency/{freq}")
+async def frequency(freq):
+    print('Setting frequency to', float(freq))
+    _digitizer.set_lamp_frequency(float(freq))
+    return {"frequency": freq}
+
+@app.get("/digitizer/{command}")
+async def digitizer(command):
+
+    if command == 'start_capture':
+        print('start_capture')
+    elif command == 'check_capture':
+        print('check_capture')
+    elif command == 'get_data':
+        print('get_data')
+        return {"data": [1, 2, 3, 4, 5]*1000}
+        # return {"command": command}
+    elif command == 'busy':
+        print('busy')
+    elif command == 'get_data':
+        print('get_data')
+    elif command == 'get_data':
+        print('get_data')
+
+    return {"command": command}
+
+
+if __name__ == "__main__":
+
+    print('About to call lamp_on')
+    _digitizer.lamp_on()
+    print('Called lamp_on')
+    time.sleep(2)
+    print('About to call lamp_off')
+    _digitizer.lamp_off()
+    print('Called lamp_off')
+
+    _digitizer.close()
