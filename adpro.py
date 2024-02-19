117a133136395334508d85393dd1bb09c177015b
Marco Del Tutto <marco.deltutto@gmail.com>
2024-02-19 10:07:10 -0600

    Trigger pos

--- adpro.py
+++ adpro.py
@@ -14,7 +14,7 @@ class ADPro():
     _sampling_frequency = 2e6
     _buffer_size = 6000
     _amplitude_range = 100e-3
-    _n_acquisitions = 1
+    _n_acquisitions = 10
     _trigger_pos = 0.4*_buffer_size/_sampling_frequency
 
     def __init__(self):
